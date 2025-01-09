e8d2be452c24ff1bb569ac9eb7aa322d4960cd14
souzamiqueias <mqs.sza@gmail.com>
2025-01-09 13:25:20 -0300

    Remove large files and add download script
    Former-commit-id: e139267e95392b16341f1331b373032c1458d105

---
+++ download_large_files.sh
@@ -0,0 +1,2 @@
+wget -O venv/Lib/site-packages/torch/lib/torch_cpu.dll "https://drive.google.com/file/d/1zEgeeb6u0kh5iVuIH4veVnsIYqbz7ivf/view?usp=drive_link"
+wget -O venv/Lib/site-packages/torch/lib/dnnl.lib "https://drive.google.com/file/d/1VRBDD6KV9mhIVxq15M2_atnLWYccp5SL/view?usp=drive_link"
